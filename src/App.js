e4b932ea4a0b17f3861e3b22abed15044059ef49
Fion L <hwindepth@yahoo.com>
2022-01-21 20:49:04 +0000

    added picture

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="header">TIC TAC TOE</h1>
+      <p4><center>A minmax game for use at higher
+      level for Chess and the like strategy games</center></p4>
+     
       <Board />
     </div>
   );
